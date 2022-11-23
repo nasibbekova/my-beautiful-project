73c43ed9f3373c0960d51b9924e8e2bae2be8614
Madina <alimon=vam.6703@gmail.com>
2022-11-23 18:12:20 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// 1
+
+let int = prompt (' Can you write any number ').toUpperCase() .trim()
+
+if (int % 2 ===0) {
+    console.log('even')
+} else {
+    console.log('odd')
+}
+
+// 2
+
+let name = prompt ('What is your name?').toUpperCase() .trim() 
+let age = prompt ('Are you already 18?') .trim()
+
+if (name % 2 === 0) {
+    console.log('good');
+} else {
+    console.log('old')
+}
+
+// 3
+
+let  forename = prompt( 'What is your name?').toUpperCase() .trim( ) 
+let years= prompt( 'Are you upper 18?').trim( )
+
+if(forename[0] === 'A' && age === true) {
+    console.log('welcome')
+} else {
+    console.log('go home')
+}
+
+// 4
+
+let guests = prompt ('Mirziyoyev Putin Obama Biden Zelenskiy Trump')
+
+if (guests  === true) {
+    console.log ('acceptable')
+} else {
+    console.log ('not acceptable')
+}
